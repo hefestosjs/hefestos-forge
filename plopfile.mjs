11c725302f719a22dff11bf6752b0925e5203b6b
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-08-28 02:44:56 -0300

    fix: return action as an empty array

--- plopfile.mjs
+++ plopfile.mjs
@@ -3,17 +3,6 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 export default function (plop) {
-  plop.setActionType("executeScript", (answers) => {
-    let { runtime, repository } = answers;
-    runtime = runtime.toLowerCase();
-
-    const __dirname = path.dirname(fileURLToPath(import.meta.url));
-    const filePath = path.resolve(__dirname, `./src/${runtime}.js`);
-    const command = `${runtime} ${filePath} ${repository}`;
-
-    execSync(command, { stdio: "inherit" });
-  });
-
   plop.setGenerator("select-runtime", {
     description: "Choose the runtime to use (Node or Bun)",
     prompts: [
@@ -29,6 +18,17 @@ export default function (plop) {
         message: "What is the repository name?",
       },
     ],
-    actions: [{ type: "executeScript" }],
+    actions: (answers) => {
+      let { runtime, repository } = answers;
+      runtime = runtime.toLowerCase();
+
+      const __dirname = path.dirname(fileURLToPath(import.meta.url));
+      const filePath = path.resolve(__dirname, `./src/${runtime}.js`);
+      const command = `${runtime} ${filePath} ${repository}`;
+
+      execSync(command, { stdio: "inherit" });
+
+      return [];
+    },
   });
 }
